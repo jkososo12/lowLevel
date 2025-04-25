ac55d8177eda28cd2bf695b48f63a40fdf01550b
jkososo12 <kulijawaveteran@gmail.com>
2025-04-26 06:05:08 +0700

    Modified macros.c and build executable file macros

--- basic/macros.c
+++ basic/macros.c
@@ -1,13 +1,21 @@
-#inlcude <stdio.h>
+#include <stdio.h>
 #define WIDHT 80
 #define HEIGHT 25
-#undef HEIGHT 25
+#undef HEIGHT 
 #define HEIGHT 30
+#define LED_ON
+
+#ifdef LED_ON
+printf("LED is ON\n");
+#else 
+printf("LED is OFF\n");
+#endif
 
 int main(){
-  printf("File : %s", __FILE__);
-  printf("Line : %d", __LINE__);
-  printf("Date : %s", __DATE__);
-  printf("Time : %s", __TIME__);
+  printf("File : %s\n", __FILE__);
+  printf("File : %s\n", __TIME__);
+  printf("File : %s\n", __DATE__);
+  printf("File : %d\n", __STDC__); 
+  printf("File : %d\n", __LINE__); 
   return 0;
 }
