66d210b823a7f562fea82447d9327660298b80ce
jkososo12 <kulijawaveteran@gmail.com>
2025-04-25 20:23:37 +0700

    Modified Macros.c

--- basic/macros.c
+++ basic/macros.c
@@ -3,3 +3,11 @@
 #define HEIGHT 25
 #undef HEIGHT 25
 #define HEIGHT 30
+
+int main(){
+  printf("File : %s", __FILE__);
+  printf("Line : %d", __LINE__);
+  printf("Date : %s", __DATE__);
+  printf("Time : %s", __TIME__);
+  return 0;
+}
