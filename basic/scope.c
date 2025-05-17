78174b3f8764b08b5148e4448a75aa71933b4768
jkososo12 <kulijawaveteran@gmail.com>
2025-05-17 17:36:06 +0700

    modified scope.c

--- basic/scope.c
+++ basic/scope.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
-int global = 10;
+int global = 10; // bisa di akses di mana saja
+int z = 51; // tidak bisa di akses karena sudah ada variable dengan nama sama di block function main
 
 int main(){
-  int local = 20;
+  int local = 20; // hanya bisa di function main / block ini
+  int z = 60; // sama kaya variable local
   printf("Ini adalah variable global : %d\n", global);
   printf("Ini adalah variable local : %d\n", local);
-  if(int x = 30){
+  printf("Ini adalah variable z : %d\n", z);
+  if(local >= 30){ // cuman bisa di akses dari if ini
     int b = 40;
     printf("Variable x adalah variable parameter : %d\n", x);
     printf("Variable b adalah variable block : %d", b);
