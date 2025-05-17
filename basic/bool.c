9e3c666c7727c16ccdf5d8b424de01c362b94ecf
jkososo12 <kulijawaveteran@gmail.com>
2025-05-17 17:38:30 +0700

    new file bool.c and bool

---
+++ basic/bool.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    bool isTrue = true;
+    bool isFalse = false;
+
+    if (isTrue) {
+        printf("isTrue is true\n");
+    } else {
+        printf("isTrue is false\n");
+    }
+
+    if (isFalse) {
+        printf("isFalse is true\n");
+    } else {
+        printf("isFalse is false\n");
+    }
+
+    return 0;
+}
